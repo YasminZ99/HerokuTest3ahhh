66e62ef1aeedc0f5fd0b6015cfb3cbdb8f656ec6
YasminZ99 <yasmin.ziegler@smail.th-koeln.de>
2021-02-18 11:45:45 +0100

    h

--- ok.js
+++ ok.js
@@ -1,7 +1,5 @@
-const express = require("express");
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 5000;
 
-const PORT = process.env.PORT || 3000;
-
-express.listen(process.env.PORT || 3000)
-
-console.log("hallo");
+express.listen(PORT, () => console.log(`Listening on ${ PORT }`));
